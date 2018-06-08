83ec1f5ada3ee807289c657c0e10d734bc2189d7
Jonas Johansson <jonasj76@gmail.com>
2018-06-08 10:19:44 +0200

    fix insecure temporary file
    Signed-off-by: Jonas Johansson <jonasj76@gmail.com>

--- src/misc.c
+++ src/misc.c
@@ -67,8 +67,11 @@ int setup_nftables(cfg_t *cfg)
 	struct nl_sock *sk;
 	int err = 0;
 	int fd;
+	mode_t cur_umask;
 
+	cur_umask = umask(0600);
 	fd = mkstemp(file);
+	umask(cur_umask);
 	if (fd == -1) {
 		syslog2(LOG_ERR, "Could not generate tempfile");
 		return 1;
